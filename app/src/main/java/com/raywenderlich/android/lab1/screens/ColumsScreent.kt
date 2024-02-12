fe07e176813da4b9c41fed64f1053a66451fb7b9
soscooter <lelya.rumynin@gmail.com>
2024-02-13 00:00:22 +0300

    add column screen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ColumsScreent.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ColumsScreent.kt
@@ -1,12 +1,11 @@
 package com.raywenderlich.android.lab1.screens
 
 import androidx.compose.runtime.Composable
-import com.raywenderlich.android.lab1.R
+import com.raywenderlich.android.lab1.router.BackButtonHandler
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,16 +13,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.router.Screen
 
-val THREE_ELEMENTS_LIST = listOf(R.string.first, R.string.second, R.string.third)
-
 @Composable
-fun RowScreen() {
-    myRow()
+fun ColumnScreen(){
+    MyColumn()
+
     BackButtonHandler {
         FundamentalsRouter.navigateTo(Screen.Navigation)
     }
 }
+
 @Composable
-fun myRow() {
+fun MyColumn() {
 
 }
