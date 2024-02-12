2d34b4001a8c1d1371e4d26ab93cb577fdf1ca36
soscooter <lelya.rumynin@gmail.com>
2024-02-13 00:50:42 +0300

    complete myRow

--- app/src/main/java/com/raywenderlich/android/lab1/screens/RowScreent.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/RowScreent.kt
@@ -1,5 +1,6 @@
 package com.raywenderlich.android.lab1.screens
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.R
 import androidx.compose.foundation.layout.Arrangement
@@ -10,11 +11,18 @@ import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.router.Screen
 
-val THREE_ELEMENTS_LIST = listOf(R.string.first, R.string.second, R.string.third)
+
+
+val THREE_ELEMENTS_LIST = listOf(
+    R.string.first,
+    R.string.second,
+    R.string.third
+)
 
 @Composable
 fun RowScreen() {
@@ -25,5 +33,19 @@ fun RowScreen() {
 }
 @Composable
 fun myRow() {
+
+    Row (verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()) {
+
+        THREE_ELEMENTS_LIST.forEach { textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 18.sp
+            )
+
+        }
+
+    }
 
 }
